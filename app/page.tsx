3d08a4b3889f6575b96b4676ad3a7da3ed540a55
TomKim <babooteng2@gmail.com>
2024-04-09 06:20:51 +0900

    Tailwind #3.6 Form Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,34 @@ export default function Home() {
       xl:bg-orange-100      
       2xl:bg-purple-100
   ">
-    <div className="flex flex-col md:flex-row gap-2 bg-white w-full shadow-lg p-5 rounded-2xl max-w-screen-sm">
-      <input className="w-full rounded-full pl-5 bg-gray-100 py-3 ring ring-transparent outline-none 
-        focus:ring-orange-500
+    <div
+      className="flex flex-col md:flex-row gap-2 bg-white w-full shadow-lg p-5 rounded-2xl 
+        max-w-screen-sm">
+      <input
+        className="w-full rounded-full pl-5 bg-gray-100 py-3 ring ring-transparent outline-none 
+        focus:ring-green-500
           focus:ring-offset-2
-          transition-shadow          
-        " type="text" placeholder="Search here..." />
-      <button className="bg-black font-medium text-white py-2 rounded-full active:scale-95 
+          transition-shadow
+          placeholder:drop-shadow
+          invalid:focus:ring-red-500
+          peer"
+        type="email"
+        required
+        placeholder="Email address"
+      />
+      <span className="hidden peer-invalid:block
+        md:absolute md:pt-12 md:ml-5 text-center text-red-500 font-medium"
+      >
+        Email is required
+      </span>
+      <button className="font-medium text-white py-2 rounded-full active:scale-95 hover:scale-105 text-nowrap
         transition-transform outline-none
         md:px-10
-      ">Search</button>
+        bg-black
+        peer-invalid:bg-red-500
+        peer-required:bg-green-500">
+        Log in
+      </button>
     </div>
   </main>;
 }
