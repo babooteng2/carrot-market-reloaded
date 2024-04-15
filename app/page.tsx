d2c283d8a7c12d68ed1ffdb009027434c3d6bd59
TomKim <babooteng2@gmail.com>
2024-04-16 08:09:29 +0900

    Tailwind #3.12 Plugins

--- app/page.tsx
+++ app/page.tsx
@@ -11,34 +11,29 @@ export default function Home() {
       className="flex flex-col md:flex-row gap-3 bg-white w-full shadow-lg p-5 rounded-2xl 
         max-w-screen-sm
     ">
-      <div className="group flex flex-col w-full gap-4">
-        <input className="bg-gray-100" />
-        <span className="group-focus-within:block hidden">
-          Make sure it is a valid email...
-        </span>
-        <a href="#">aaa</a>
+      <div className="flex flex-col w-full gap-4">
+        <input className="          
+          form-input
+        " />
+        <input type="date" name="" id="" className="form-input" />
+        <select name="" id="" className="form-select">
+          <option value="">Copporate event</option>
+          <option value="">Wedding</option>
+          <option value="">Birthday</option>
+          <option value="">Other</option>
+        </select>
+        <textarea className="form-textarea" />
+        <div>
+          <label className="inline-flex items-center">
+            <input type="checkbox" name="" id="" className="form-checkbox" />
+            <span className="ml-2">Email me news and special offers</span>
+          </label>
+        </div>
         <button className="
           btn
           text-bigger-hello  
         ">Submit</button>
       </div>
-      {/* {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
-        <div key={index} className="flex items-center gap-5 group">          
-          <div className="size-10 bg-blue-400 rounded-full" />
-          <span className="text-lg font-medium
-            group-hover:text-red-500
-            empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300
-          ">
-            {person}
-          </span>
-          <div className="size-6
-            bg-red-500 text-white flex items-center justify-center rounded-full relative
-          ">
-            <span className="z-10">{index}</span>
-            <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-          </div>
-        </div>
-      ))} */}
     </div>
   </main>;
 }
