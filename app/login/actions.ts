9face9e50063c65e199e2bf7fcf2e15da27ab623
TomKim <babooteng2@gmail.com>
2024-05-07 19:00:08 +0900

    #5.3 useFormState

---
+++ app/login/actions.ts
@@ -0,0 +1,12 @@
+"use server"
+
+export async function handleForm(prevState: any, formData: FormData) {
+    await new Promise((resolve) => setTimeout
+        (resolve, 5000));
+    console.log("i run in the server only baby",
+        formData.get("email"), formData.get("password"));
+    console.log("in server", prevState);
+    return {
+        errors: ["wrong password", "password too short"]
+    }
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,22 @@
+"use client";
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { handleForm } from "./actions";
 
 export default function LogIn() {
-    const handleForm = async (formData: FormData) => {
-        "use server";
-        await new Promise((resolve) => setTimeout
-            (resolve, 5000));
-        console.log("i run in the server only baby",
-            formData.get("email"), formData.get("password"));
 
-    }
+    const [state, action] = useFormState(handleForm, null);
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form action={handleForm} className="flex flex-col gap-3">
+            <form action={action} className="flex flex-col gap-3">
                 <FormInput type="text" placeholder="Email" required errors={[]} name="email" />
-                <FormInput type="text" placeholder="Password" required errors={[]} name="password" />
+                <FormInput type="text" placeholder="Password" required errors={state?.errors ?? []} name="password" />
                 <FormButton text="Log in" />
             </form>
             <SocialLogin />
