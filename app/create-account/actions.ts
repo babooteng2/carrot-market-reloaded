2ea22cc49a8e326e0eca50e33dad741076bc9eda
TomKim <babooteng2@gmail.com>
2024-05-10 18:55:35 +0900

    #6.2 Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,33 @@
 "use server"
 import { z } from "zod"
 
-const fromSchema = z.object({
-  userName: z.string().min(5).max(10),
-  email: z.string().email(),
-  password: z.string().min(8),
-  confirmPassword: z.string().min(8),
-})
+const checkUsername = (userName: string) => !userName.includes("potato")
+const checkPasswords = ({
+  password,
+  confirmPassword,
+}: {
+  password: string
+  confirmPassword: string
+}) => password === confirmPassword
+
+const fromSchema = z
+  .object({
+    userName: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Username is required",
+      })
+      .min(5, "Way too short!!!")
+      .max(10)
+      .refine((userName) => checkUsername, "No potatoes allowed"),
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8),
+  })
+  .refine(checkPasswords, {
+    path: ["confirmPassword"],
+    message: "Both passwords should be the same",
+  })
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -17,7 +38,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   }
   const result = fromSchema.safeParse(data)
   if (!result.success) {
-    // console.log("validation failed : ", result.error.flatten())
+    console.log("validation failed : ", result.error.flatten())
     return result.error.flatten()
   }
 }
