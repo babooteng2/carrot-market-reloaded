35353e682c376672cd56e87f7a21d0eb966e634c
TomKim <babooteng2@gmail.com>
2024-05-12 17:50:25 +0900

    #6.3 Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,9 @@
 "use server"
 import { z } from "zod"
 
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[*\d])(?=.*?[!@#$%^&*-?]).+$/
+)
 const checkUsername = (userName: string) => !userName.includes("potato")
 const checkPasswords = ({
   password,
@@ -17,11 +20,22 @@ const fromSchema = z
         invalid_type_error: "Username must be a string!",
         required_error: "Username is required",
       })
-      .min(5, "Way too short!!!")
+      .min(5, "최소 다섯자리 이상 입력해야 합니다.")
       .max(10)
+      .toLowerCase()
+      .trim()
+      .transform((userName) =>
+        userName.includes("flameable") ? `🔥 ${userName}` : userName
+      )
       .refine((userName) => checkUsername, "No potatoes allowed"),
-    email: z.string().email(),
-    password: z.string().min(8),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(8)
+      .regex(
+        passwordRegex,
+        "비밀번호는 영문 소문자, 영문 대문자, 특수문자를 각 한개 이상 포함해야 합니다."
+      ),
     confirmPassword: z.string().min(8),
   })
   .refine(checkPasswords, {
@@ -37,8 +51,11 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirmPassword: formData.get("confirmPassword"),
   }
   const result = fromSchema.safeParse(data)
+
   if (!result.success) {
-    console.log("validation failed : ", result.error.flatten())
+    //console.log("validation failed : ", result.error.flatten())
     return result.error.flatten()
+  } else {
+    console.log(result.data)
   }
 }
