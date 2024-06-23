eaeb4adbb88f8295af36f915cfe314fc178d5961
TomKim <babooteng2@gmail.com>
2024-06-23 17:01:12 +0900

    #9.3 Github API

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,4 +1,7 @@
-import { notFound } from "next/navigation"
+import db from "@/app/lib/db"
+import getSession from "@/app/lib/session"
+import { log } from "console"
+import { notFound, redirect } from "next/navigation"
 import { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
@@ -16,9 +19,42 @@ export async function GET(request: NextRequest) {
     method: "POST",
     headers: { Accept: "application/json" },
   })
-  const accessTokenData = await accessTokenRespone.json()
-  if ("error" in accessTokenData) {
+  const { error, access_token } = await accessTokenRespone.json()
+  if (error) {
     return new Response(null, { status: 400 })
   }
-  return Response.json({ accessTokenData })
+  const userProfileResponse = await fetch("https://api.github.com/user", {
+    headers: { Authorization: `Bearer ${access_token}` },
+    cache: "no-cache",
+  })
+  const { id, avatar_url, login } = await userProfileResponse.json()
+  const user = await db.user.findUnique({
+    where: {
+      github_id: id + "",
+    },
+    select: {
+      id: true,
+    },
+  })
+  if (user) {
+    const session = await getSession()
+    session.id = user.id
+    await session.save()
+    return redirect("/profile")
+  }
+  // todo : username은 필수 항목인데 새사용자 생성시 같은이름이 존재할 때 처리필요
+  const newUser = await db.user.create({
+    data: {
+      username: login,
+      github_id: id + "",
+      avatar: avatar_url,
+    },
+    select: {
+      id: true,
+    },
+  })
+  const session = await getSession()
+  session.id = newUser.id
+  await session.save()
+  return redirect("/profile")
 }
