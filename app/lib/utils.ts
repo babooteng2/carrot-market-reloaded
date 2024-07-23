00d01f92f1bcd32656c7d3f65d4f45f0e263f0e6
TomKim <babooteng2@gmail.com>
2024-07-24 00:48:23 +0900

    10.4 Detail Skeleton

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -53,3 +53,17 @@ export const getUserEmail = async (req: NextRequest, access_token: string) => {
   const githubUserEmails: IGithubUserEmail = await userEmails.json()
   return githubUserEmails
 }
+
+export function formatToTimeAgo(date: string): string {
+  const dayInMs = 1000 * 60 * 60 * 24
+  const time = new Date(date).getTime()
+  const now = new Date().getTime()
+  const diff = Math.round((time - now) / dayInMs)
+  const formatter = new Intl.RelativeTimeFormat("ko")
+
+  return formatter.format(diff, "days")
+}
+
+export function formatToWon(price: number): string {
+  return price.toLocaleString("ko-KR")
+}
