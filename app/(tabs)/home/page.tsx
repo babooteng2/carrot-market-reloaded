417fd80f7b92299e90091c4a88dcb72b7b048fe2
TomKim <babooteng2@gmail.com>
2025-11-29 23:17:37 +0900

    13.7 Route Segment Config

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link"
 
 export const metadata = { title: "Home" }
 
+export const dynamic = "force-dynamic"
+
 export default async function Products() {
   const initialProducts = await getCachedProducts()
   return (
