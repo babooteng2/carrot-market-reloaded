0d3e5fc1134e74ca053fe7f5ad33a447f3526e98
TomKim <babooteng2@gmail.com>
2025-12-02 17:55:04 +0900

    14.1 See Posts

--- lib/db.ts
+++ lib/db.ts
@@ -152,3 +152,22 @@ export const modifyProductById = async (
   })
   return product
 }
+
+export async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  })
+  return posts
+}
